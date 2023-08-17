752af824619c2e508b99a43f249c9f2d09c3501f
Vishal <vishaljain7100@gmail.com>
2023-08-17 21:19:58 +0530

    Search bar changes and resposive change

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const apikey = "0bde84dc9ee486d46395d1f2835a979f";
 const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-let btn = document.querySelector(".btn");
 let input = document.querySelector(".search");
 const weatherIcon = document.querySelector(".weather-icon");
 
-btn.addEventListener("click", async () => {
-  let city = input.value;
-  input.value = "";
-  checkWeather(city);
+input.addEventListener("keypress", async (event) => {
+  if (event.key == "Enter") {
+    let city = input.value;
+    input.value = "";
+    checkWeather(city);
+  }
 });
 
 async function checkWeather(city) {
   let resp = await fetch(url + city + `&appid=${apikey}`);
   let data = await resp.json();
 
-  console.log(data);
-
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".tempre").innerHTML =
-    Math.round(data.main.temp) + "°c";
+  Math.round(data.main.temp) + "°c";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
